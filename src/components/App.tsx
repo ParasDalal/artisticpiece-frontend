ccda58ca475218bd0a3d5543e1ef984d5b4a18c8
Paras Dalal <paras@dalalinc.com>
2021-07-30 15:57:27 -0400

    router logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import "../Resources/App.css";
 import { Header } from "./Header";
@@ -10,25 +10,10 @@ function AppRouter() {
      return (
        <div>
        <Header />
-       <Router>
-         <div>
-           <nav>
-             <a href="/">Home</a>
-             <a href="/Training">Training</a>
-             <ul>
-               <li>
-                 <Link to="/">Home</Link>
-               </li>
-               <li>
-                 <Link to="/training">Training</Link>
-               </li>
-             </ul>
-           </nav>
-   
-           <Route path="/" exact component={Home} />
-           <Route path="/training" exact component={Training} />
-         </div>
-       </Router>
+     <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/training" exact component={Training} />
+        </Router>
        </div>
      );
   }
