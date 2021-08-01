072861589590623c20b7e5d7ec0630c12a7e210a
Paras Dalal <info@dalalinc.com>
2021-08-01 15:05:42 -0400

    menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import "../Resources/App.css";
 import { Header } from "./Header";
-import { Home } from "../pages/Home";
-import { Training } from "../pages/Training";
 
 function AppRouter() {
-     return (
-       <div>
-       <Header />
-     <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/training" exact component={Training} />
-        </Router>
-       </div>
-     );
-  }
-   
-export default AppRouter;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
 
+export default AppRouter;
 
 // function App() {
 //   return (
-    
 
 //     <div className="App">
 //       <Header />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 import { UserIcon } from "./Icons";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { Home } from "../pages/Home";
+import { Training } from "../pages/Training";
+import { Login } from "../pages/Login";
 
 export const Header = () => (
   <div>
-    <a href="./">ArtisticPiece</a>
-    <a href="./Training">Training</a>
-    <a href="./signin">
-      <UserIcon />
-      <span>Sign In</span>
-    </a>
+    <Router>
+      <Navbar bg="primary" expand="lg" variant="dark">
+        <Navbar.Brand href="#home">ArtisticPiece</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav variant="pills">
+            <Nav.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/Training">Training</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/Login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Route path="/" exact component={(props: any) => <Home {...props} />} />
+    </Router>
   </div>
 );
